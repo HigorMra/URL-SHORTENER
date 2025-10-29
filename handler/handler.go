9a34281d536aa31333045ad514308604ab035dcf
Higor Moreira <167116708+HigorMra@users.noreply.github.com>
2025-10-29 18:15:22 -0300

    Update handler.go

--- handler/handler.go
+++ handler/handler.go
@@ -7,18 +7,15 @@ import (
 	"time"
 )
 
-// storage simula um banco de dados usando um mapa em memória
 var urlStore = make(map[string]string)
 
-// InitStore adiciona alguns dados de teste
 func InitStore() {
-	// Chave Curta: URL Longa
+	
 	urlStore["go-docs"] = "https://go.dev/doc/"
 	urlStore["github"] = "https://github.com/google/go-cloud"
 	fmt.Println("URLs de teste carregadas.")
 }
 
-// RedirectURLHandler lida com a requisição de redirecionamento (GET /<short-url>)
 func RedirectURLHandler(w http.ResponseWriter, r *http.Request) {
 	shortURL := strings.TrimPrefix(r.URL.Path, "/")
 
@@ -33,14 +30,12 @@ func RedirectURLHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Printf("[%s] Redirecionamento para: %s\n", time.Now().Format("15:04:05"), longURL)
 }
 
-// ShortenURLHandler lida com a requisição de criação (POST /shorten)
 func ShortenURLHandler(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodPost {
 		http.Error(w, "Método não permitido", http.StatusMethodNotAllowed)
 		return
 	}
 
-	// Simplificação: vamos apenas adicionar um novo par fixo
 	newShortKey := "go-home"
 	newLongURL := "https://go.dev/"
 
