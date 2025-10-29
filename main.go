a86a8d0629a2b1bb3c322ce14b45c6e6758e8450
Higor Moreira <167116708+HigorMra@users.noreply.github.com>
2025-10-29 18:16:53 -0300

    Update main.go

--- main.go
+++ main.go
@@ -4,22 +4,19 @@ import (
 	"fmt"
 	"log"
 	"net/http"
-	"url-shortener/handler" // Importa o nosso pacote local
+	"url-shortener/handler" 
 )
 
 func main() {
 	const port = ":8080"
 	
-	// Inicializa o armazenamento de teste no nosso pacote handler
 	handler.InitStore()
 
-	// 1. Rota para criar novas URLs
 	http.HandleFunc("/shorten", handler.ShortenURLHandler)
 
-	// 2. Rota para redirecionar URLs (a rota principal '/')
-	// Se o caminho não for /shorten, ele tenta redirecionar
 	http.HandleFunc("/", handler.RedirectURLHandler)
 
 	fmt.Printf("Servidor iniciado na porta %s...\n", port)
 	log.Fatal(http.ListenAndServe(port, nil))
 }
+
